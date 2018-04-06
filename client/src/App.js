ed3aec9e2c4902f755176e0fc67225245339909b
Cesar Mejia Leiva <cmejia@gmail.com>
2018-04-06 10:49:15 -0700

    client-server are talking; server error handling todo

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Button } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getProjects();
+  }
+
+  getProjects() {
+    axios
+      .get('http://localhost:5000/api/projects')
+      .then(res => {
+        this.setState({ projects: res.data });
+        console.log(res.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <Button color="danger">Danger</Button>
+        {this.state.projects.map(project => (
+          <Button color="primary">{project.name}</Button>
+        ))}
       </div>
     );
   }
